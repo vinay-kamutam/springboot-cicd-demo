76f7260f49a7ee2efca80a8dc33f5e93d5044c6a
vinay-kamutam <67751404+vinay-kamutam@users.noreply.github.com>
2026-07-25 03:52:05 +0530

    Add files via upload

---
+++ src/test/java/com/example/springboot_cicd_demo/SpringbootCicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_cicd_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
