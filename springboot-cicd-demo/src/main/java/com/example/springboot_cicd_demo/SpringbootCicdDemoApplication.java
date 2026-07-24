f4843b59bdef487f8e93bf9a6251d76b0012caa8
vinay-kamutam <67751404+vinay-kamutam@users.noreply.github.com>
2026-07-25 03:41:17 +0530

    Add files via upload

---
+++ springboot-cicd-demo/src/main/java/com/example/springboot_cicd_demo/SpringbootCicdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_cicd_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCicdDemoApplication.class, args);
+	}
+
+}
