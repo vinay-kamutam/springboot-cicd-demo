320d3d2ddf5de34aeac8c82dfdbdb861df9a8ae4
vinay-kamutam <67751404+vinay-kamutam@users.noreply.github.com>
2026-07-30 20:05:01 +0530

    Update HomeController.java

--- src/main/java/com/example/springboot_cicd_demo/controller/HomeController.java
+++ src/main/java/com/example/springboot_cicd_demo/controller/HomeController.java
@@ -1,17 +1,19 @@
 package com.example.springboot_cicd_demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Spring Boot CI/CD Project!";
+        return "index";   // Loads templates/index.html
     }
 
     @GetMapping("/health")
+    @ResponseBody
     public String health() {
         return "Application is running successfully!";
     }
